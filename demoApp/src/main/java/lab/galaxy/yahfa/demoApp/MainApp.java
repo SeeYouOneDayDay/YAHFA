e2e03e0c0065ab551152f51ba2c9dee6e8a78906
sanbo <sanbo.xyz@gmail.com>
2022-09-19 18:33:35 +0800

    去除黑灰名单约束。修改demo编译就更新

--- demoApp/src/main/java/lab/galaxy/yahfa/demoApp/MainApp.java
+++ demoApp/src/main/java/lab/galaxy/yahfa/demoApp/MainApp.java
@@ -2,9 +2,16 @@ package lab.galaxy.yahfa.demoApp;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.os.Build;
 import android.util.Log;
 
+import org.lsposed.hiddenapibypass.HiddenApiBypass;
+
+import java.io.Closeable;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 import dalvik.system.DexClassLoader;
 import lab.galaxy.yahfa.HookMain;
@@ -18,19 +25,62 @@ public class MainApp extends Application {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
 
+        if (Build.VERSION.SDK_INT > 27) {
+            HiddenApiBypass.unseal(this);
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
         try {
-        /*
-        Build and put the demoPlugin apk in sdcard before running the demoApp
-         */
+            String sourceName = "demoPlugin-debug.apk";
+            extractAssets(this, sourceName);
+
             ClassLoader classLoader = getClassLoader();
             DexClassLoader dexClassLoader = new DexClassLoader(
 //                    new File(Environment.getExternalStorageDirectory(), "demoPlugin-debug.apk").getAbsolutePath(),
-                    new File("/sdcard/demoPlugin-debug.apk").getAbsolutePath(),
+                    getFileStreamPath(sourceName).getAbsolutePath(),
                     getCodeCacheDir().getAbsolutePath(), null, classLoader);
             Log.d("YAHFA.app", "dexClassLoader:" + dexClassLoader);
             HookMain.doHookDefault(dexClassLoader, classLoader);
         } catch (Throwable e) {
             Log.e("YAHFA.app", Log.getStackTraceString(e));
+        }
+    }
+
+    public static void extractAssets(Context context, String sourceName) {
+        AssetManager am = context.getAssets();
+        InputStream is = null;
+        FileOutputStream fos = null;
+        try {
+            is = am.open(sourceName);
+            File extractFile = context.getFileStreamPath(sourceName);
+            fos = new FileOutputStream(extractFile);
+            byte[] buffer = new byte[1024];
+            int count = 0;
+            while ((count = is.read(buffer)) > 0) {
+                fos.write(buffer, 0, count);
+            }
+            fos.flush();
+        } catch (Throwable e) {
+            Log.e("YAHFA.app", Log.getStackTraceString(e));
+        } finally {
+            closeSilently(is);
+            closeSilently(fos);
+        }
+
+    }
+
+    private static void closeSilently(Closeable closeable) {
+        if (closeable == null) {
+            return;
+        }
+        try {
+            closeable.close();
+        } catch (Throwable e) {
+            // ignore
         }
     }
 }
